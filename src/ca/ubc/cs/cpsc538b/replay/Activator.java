43900b34495c761e8551fd6cb5beeda09daf94d8
daniel Rozenberg <daniboy@gmail.com>
2014-03-08 19:31:24 -0800

    Stops recording when switching to the Replay perspective

--- src/ca/ubc/cs/cpsc538b/replay/Activator.java
+++ src/ca/ubc/cs/cpsc538b/replay/Activator.java
@@ -2,26 +2,33 @@ package ca.ubc.cs.cpsc538b.replay;
 
 import org.eclipse.core.commands.Command;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.NotEnabledException;
+import org.eclipse.core.commands.NotHandledException;
 import org.eclipse.core.commands.State;
+import org.eclipse.core.commands.common.NotDefinedException;
 import org.eclipse.ui.IPerspectiveDescriptor;
 import org.eclipse.ui.IPerspectiveListener;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.commands.ICommandService;
-import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.handlers.IHandlerService;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends AbstractUIPlugin {
+public class Activator extends AbstractUIPlugin implements IPerspectiveListener {
 
     // The plug-in ID
     public static final String PLUGIN_ID = "ca.ubc.cs.cpsc538b.replay";
 
     // The shared instance
     private static Activator plugin;
+
+    private ICommandService commandService;
+
+    private IHandlerService handlerService;
 
     public Activator() {
     }
@@ -30,8 +37,16 @@ public class Activator extends AbstractUIPlugin {
     public void start(BundleContext context) throws Exception {
         super.start(context);
 
-        PlatformUI.getWorkbench().getActiveWorkbenchWindow()
-                .addPerspectiveListener(new ControlRecordingPerspectiveListener());
+        PlatformUI.getWorkbench().getActiveWorkbenchWindow().addPerspectiveListener(this);
+
+        commandService = (ICommandService) PlatformUI.getWorkbench().getService(ICommandService.class);
+        handlerService = (IHandlerService) PlatformUI.getWorkbench().getService(IHandlerService.class);
+
+        // Set the toggle buttons to be unselected at startup
+        commandService.getCommand("ca.ubc.cs.cpsc538b.replay.commands.record")
+                .getState("org.eclipse.ui.commands.toggleState").setValue(Boolean.FALSE);
+        commandService.getCommand("ca.ubc.cs.cpsc538b.replay.commands.play")
+                .getState("org.eclipse.ui.commands.toggleState").setValue(Boolean.FALSE);
 
         plugin = this;
     }
@@ -51,27 +66,23 @@ public class Activator extends AbstractUIPlugin {
         return plugin;
     }
 
-    private static final class ControlRecordingPerspectiveListener implements IPerspectiveListener {
+    @Override
+    public void perspectiveChanged(IWorkbenchPage page, IPerspectiveDescriptor perspective, String changeId) {
+    }
 
-        @Override
-        public void perspectiveChanged(IWorkbenchPage page, IPerspectiveDescriptor perspective, String changeId) {
-        }
+    @Override
+    public void perspectiveActivated(IWorkbenchPage page, IPerspectiveDescriptor perspective) {
+        // When switching to the Replay perspective, stop the recording
+        if (perspective.getId().equals("ca.ubc.cs.cpsc538b.replay.perspective")) {
+            Command recordingCommand = commandService.getCommand("ca.ubc.cs.cpsc538b.replay.commands.record");
+            State state = recordingCommand.getState("org.eclipse.ui.commands.toggleState");
+            boolean isRecording = (boolean) state.getValue();
 
-        @Override
-        public void perspectiveActivated(IWorkbenchPage page, IPerspectiveDescriptor perspective) {
-            if (false /* TODO doesn't really stop the recording, fix this */&& perspective.getId().equals(
-                    "ca.ubc.cs.cpsc538b.replay.perspective")) {
-                ICommandService service = (ICommandService) PlatformUI.getWorkbench().getService(ICommandService.class);
-                Command command = service.getCommand("ca.ubc.cs.cpsc538b.replay.commands.record");
-                State state = command.getState("org.eclipse.ui.commands.toggleState");
-                boolean isRecording = (boolean) state.getValue();
-
-                if (isRecording) {
-                    try {
-                        HandlerUtil.toggleCommandState(command);
-                    } catch (ExecutionException e) {
-                        e.printStackTrace();
-                    }
+            if (isRecording) {
+                try {
+                    handlerService.executeCommand(recordingCommand.getId(), null);
+                } catch (ExecutionException | NotDefinedException | NotEnabledException | NotHandledException e) {
+                    e.printStackTrace();
                 }
             }
         }
